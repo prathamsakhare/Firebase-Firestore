cc3e6581ce1543b3decc52ae8f5ca99391dbdb37
PrathamSakhare <psakhre@themohh.com>
2023-08-01 00:14:50 +0530

    connected to firestore and added functionality of making documents and making collections inside it -- writeData in firestore /@prathamsakhare

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import {getFirestore, collection, addDoc} from 'firebase/firestore'
 import './App.css';
+import {app} from './firebase'
+
+const firestore = getFirestore(app)
 
 function App() {
+
+  const writeData = async () => {
+    const result = await addDoc(collection(firestore, 'cities'), {
+      name : "Delhi",
+      pinCode : 1234,
+      lat : 123,
+      long : 456
+    })
+    console.log(result)
+  }
+
+  // ! addDoc helps you to add a document in firestore
+  // * whereas, for collection function, first parameter is the refernce of firestore and the path of the collection {usually name} and the second parameter is its details
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Firebase FireStore</h1>
+        <button onClick={writeData}>Write Data in Firestore</button>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzYU3Bg2uAewb21GWIHcC_qFfCefb02Tg",
+  authDomain: "fir-series-3.firebaseapp.com",
+  projectId: "fir-series-3",
+  storageBucket: "fir-series-3.appspot.com",
+  messagingSenderId: "124173608716",
+  appId: "1:124173608716:web:c1822aa6745b2fc46a8b29"
+};
+
+export const app = initializeApp(firebaseConfig);
